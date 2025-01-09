5b31c5737f208456900422d207ae1e29d5af95d2
Parth Maheshwari <148962439+Parth-github3@users.noreply.github.com>
2025-01-09 16:05:45 +0530

    Update RAG.py

--- RAG.py
+++ RAG.py
@@ -60,7 +60,7 @@ system_prompt = (
     "You are an assistant for question-answering tasks. "
     "Use the following pieces of retrieved context to answer the question.  "
     "If you don't know the answer, say that you "
-    "don't know. Use three sentences maximum and keep the "
+    "don't know. Use minimum of three sentences and keep the "
     "answer concise."
     "\n\n"
     "{context}"
