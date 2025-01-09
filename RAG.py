66f86681a6899a2cdb98e378658951741dc92f55
Parth Maheshwari <148962439+Parth-github3@users.noreply.github.com>
2025-01-09 14:31:43 +0530

    Removal of chroma

--- RAG.py
+++ RAG.py
@@ -9,7 +9,6 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.chains import create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.prompts import ChatPromptTemplate
-import chromadb
 from langchain_community.vectorstores import FAISS
 from dotenv import load_dotenv
 load_dotenv()
@@ -35,10 +34,6 @@ docs = text_splitter.split_documents(data)
 
 vectorstore = FAISS.from_documents(documents=docs, embedding=GoogleGenerativeAIEmbeddings(model="models/embedding-001"))
 
-# client = chromadb.Client()
-# collection = client.get_collection(name="chroma_docs")
-# results = collection.get(ids=["page"])["documents"]
-# print(results) # Not found []
 
 # Creating the retriever object to retrieve the data directly from the Vectorstore
 retriever = vectorstore.as_retriever(search_type="similarity", search_kwargs={"k": 3})
